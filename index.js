2301dad6e7f6c4a2c1507c5ec31ed495f8785cba
Shelly9457 <96901490+Shelly9457@users.noreply.github.com>
2023-01-13 10:03:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function threeline() {
+    const threeline = document.querySelector('.threeline')
+    const menu = document.querySelector('.navber__menu-hidden')
+    threeline.classList.toggle('active')
+    menu.classList.toggle('active')
+    console.log(menu);
+}
